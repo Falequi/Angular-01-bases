23590a8298a975f5c384d88c0a866f2a69e9fbef
Fabio Alejandro Quintero <fabio.quintero@gmail.com>
2023-01-13 14:47:29 -0500

    Heroe.component: create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title:  string =  'Contador App';
-  numero: number =  10;
-  base:   number =  5;
-
-  acumular( valor: number){
-    this.numero += valor;
-  }
+ 
 
   
 }
